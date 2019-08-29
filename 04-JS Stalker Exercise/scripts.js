f0d4a1007d7f7ae0e08df4798abc441d637f9990
yiting-github <w6812763cm@gmail.com>
2019-08-29 22:51:46 +0800

    Prompt and Console log Exercise

---
+++ 04-JS Stalker Exercise/scripts.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("What's your first name?");
+var userLastName = prompt("What's your last name?");
+var userAge = prompt("How old are you?");
+console.log("Hello, " + userFirstName + " " + userLastName+"!");
+console.log("You are " + userAge +" years old.")
