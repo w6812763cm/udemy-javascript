c1e5037b69361b1adcf76b4a4158064566f24fcd
yiting-github <w6812763cm@gmail.com>
2019-10-02 22:18:41 +0800

    todo list part 2

--- 11-Arrays/03-toDoList.js
+++ 11-Arrays/03-toDoList.js
@@ -9,16 +9,38 @@ window.setTimeout(function() {
     while(input !== "quit"){
         //處理input
         if(input === "list"){ //如果是List則印出todos
-            console.log(todos);
-        }
-        else if (input === "new"){ //new的話則新增到todos
-        var item = prompt("Enter new todo");
-            todos.push(item);
+            listTodos();
+
+        } else if (input === "new"){ //new的話則新增到todos
+            addTodo();
+            
+        } else if (input === "delete"){ //ask for index to be deleted
+            delTodo();
         }
         //重複詢問
         input = prompt("What would you like to do?");
     }
 
     console.log("You quit the app!");
+
+    function listTodos(){
+        console.log("**********");
+        todos.forEach(function(todo, i) { //forEach , i 代表index
+            console.log(i + ": " + todo);
+        })
+        console.log("**********");
+    }
+
+    function addTodo(){
+        var item = prompt("Enter new todo");
+        todos.push(item);
+        console.log( item + " added to list");
+    }
+
+    function delTodo(){
+        var index  = prompt("Enter index");
+            todos.splice(index ,1);
+            console.log("Todo Removed");
+    }
 }, 500);
 
