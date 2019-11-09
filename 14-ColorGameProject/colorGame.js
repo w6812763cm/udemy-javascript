4b7fcd7cd4ef57b21a816efe08b0e1da24a178d1
yiting-github <w6812763cm@gmail.com>
2019-11-09 23:31:47 +0800

    colorGame Part 4: random colors

--- 14-ColorGameProject/colorGame.js
+++ 14-ColorGameProject/colorGame.js
@@ -1,17 +1,9 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.getElementById("message");    
-
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -28,6 +20,7 @@ for(i = 0; i< squares.length; i++){
         if (clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else{
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again!";
@@ -47,4 +40,25 @@ function pickColor(){
     // Random colors - Math.floor will chop off the remaining decimal point
    var random = Math.floor(Math.random() * colors.length);
    return colors[random];
+}
+
+function generateRandomColors(num){
+    //make a array
+    var arr = [];
+    //repeat num times
+    for(i =0; i<num; i++){
+        //get random colors and push away
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+    // pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    // pick a "green" from 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    // pick a "blue" from 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g +", " + b +")" ;
 }
