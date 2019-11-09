66dc2e1bb000ac3e278c7c2b565d813c37847222
yiting-github <w6812763cm@gmail.com>
2019-11-09 21:13:38 +0800

    colorGame part2

---
+++ 14-ColorGameProject/colorGame.js
@@ -0,0 +1,37 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+
+for(i = 0; i< squares.length; i++){
+    //add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+        
+    //add click listeners to suqares
+    squares[i].addEventListener("click", function(){
+    // grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+    // compare color to pickedColor 
+        if (clickedColor === pickedColor){
+            alert("Correct");
+        } else{
+            alert("Wrong");
+        }
+    });
+  
+    
+ 
+
+
+}
