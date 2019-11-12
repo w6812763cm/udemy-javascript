88b87c7e2f6ecb74ef9aae640355584f328eedfd
yiting-github <w6812763cm@gmail.com>
2019-11-12 22:55:32 +0800

    colorGame Part 6

--- 14-ColorGameProject/colorGame.js
+++ 14-ColorGameProject/colorGame.js
@@ -1,16 +1,51 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.getElementById("message");    
 var h1 = document.querySelector("h1");
-var resetButton = document.querySelector("#reset")
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-colorDisplay.textContent = pickedColor;
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    // generate new colors of 3 squares
+    colors = generateRandomColors(numSquares);
+    // pick new color frome array
+    pickedColor = pickColor();
+    // Change colorDisplay
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else{
+            squares[i].style.display = "none";
+        }
+    }
+
+});
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
 
 resetButton.addEventListener("click", function(){
     // generate new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     // pick a new random color from array
     pickedColor = pickColor();
     // Change colorDisplay to match picked color and change h1 bgcolor
