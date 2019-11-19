8755d2242507b6f1720c4e61dc3f205e1e34efac
yiting-github <w6812763cm@gmail.com>
2019-11-19 23:31:19 +0800

    Text and HTML method

---
+++ 15-JQuery/03-Method.js
@@ -0,0 +1,14 @@
+// Select Text use text()
+$("h1").text(); // "JQuery Methods Demo Page"
+$("ul").text(); // "Skittles Starburst Twix"
+$("li").text(); // "SkittlesStarburstTwix"
+
+// Change Text
+$("h1").text("Hello!");
+$("li").text("Nice to meet you"); 
+
+// Select inner HTML use html()
+$("ul").html();
+
+// Change HTML
+$("li").html("<a href='http://www.google.com'>Google</a>");
