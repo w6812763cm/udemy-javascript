ef0f349be03efb633d14f146a77a9f6a4c1f87b6
yiting-github <w6812763cm@gmail.com>
2019-11-22 16:39:55 +0800

    jQuery effects

---
+++ 15-JQuery/05-Effect.js
@@ -0,0 +1,14 @@
+
+
+$("button").on("click",function(){
+    $("div").fadeToggle(1000, function(){
+    });
+});
+
+// when click the button the div fadeOut
+//$("button").on("click",function(){
+    // if want to do something after fadeOut, put something in function, don't put the out of the function area.
+    //$("div").fadeOut(1000, function(){
+    //    $(this).remove(); // remove all code of div
+    //});
+//});
