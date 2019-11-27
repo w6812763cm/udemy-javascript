5369a3ce04961fdf918ff9e5473faa98cbf3bccb
yiting-github <w6812763cm@gmail.com>
2019-11-27 23:21:45 +0800

    todoList Project Part 2

--- 16-TodoListProject/asset/js/todos.js
+++ 16-TodoListProject/asset/js/todos.js
@@ -1,4 +1,22 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){ // this is span
+        $(this).remove(); // this is li
+    });
+    event.stopPropagation(); // stop bubbling up
+});
+
+$("input[type='text']").on("keypress", function(event){
+   if(event.which === 13){
+       // grabbing new todo text from input
+       var todoText = $(this).val();
+       $(this).val("");
+       // create a new li and add to ul
+       $("ul").append("<li><span>X</span> " + todoText + "</li>");
+   }
+})
