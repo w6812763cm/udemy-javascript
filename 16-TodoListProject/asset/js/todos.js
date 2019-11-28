15bb6a850e45c645ce626152024c06891c2eb687
yiting-github <w6812763cm@gmail.com>
2019-11-28 23:05:23 +0800

    todoList Project finish

--- 16-TodoListProject/asset/js/todos.js
+++ 16-TodoListProject/asset/js/todos.js
@@ -17,6 +17,10 @@ $("input[type='text']").on("keypress", function(event){
        var todoText = $(this).val();
        $(this).val("");
        // create a new li and add to ul
-       $("ul").append("<li><span>X</span> " + todoText + "</li>");
+       $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
    }
 })
+
+$(".fa-plus").on("click", function(){
+    $("input[type='text']").fadeToggle();
+});
